81f94ccdd4cfa8b5f9acc2fa5f7ffed7809e82da
frugiam <frugiam@oregonstate.edu>
2024-02-14 22:47:15 -0800

    std_dev.py

--- std_dev.py
+++ std_dev.py
@@ -23,11 +23,3 @@ def std_dev(person_list):
         square_sum += (mean_age - person.get_age()) ** 2
 
     return (square_sum / len(person_list)) ** 0.5
-
-
-p1 = Person("Kyoungmin", 73)
-p2 = Person("Mercedes", 24)
-p3 = Person("Beatrice", 48)
-person_list = [p1, p2, p3]
-answer = std_dev(person_list)
-print('Standard Deviation of age:', answer)

c5cb6effbb610a7ccb5d9ac92df4a934912c3af3
frugiam <frugiam@oregonstate.edu>
2024-02-14 22:49:56 -0800

    std_dev.py

--- std_dev.py
+++ std_dev.py
@@ -3,14 +3,14 @@
 # Date: 02/14/2024
 # Description: Project 6c
 
-def __init__(self, name, age):
-    self.__name = name
-    self.__age = age
+class Person():
 
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
 
-def get_age(self):
-    return self.__age
-
+    def get_age(self):
+        return self.__age
 
 def std_dev(person_list):
     total = 0
@@ -23,3 +23,10 @@ def std_dev(person_list):
         square_sum += (mean_age - person.get_age()) ** 2
 
     return (square_sum / len(person_list)) ** 0.5
+
+p1 = Person("Kyoungmin", 73)
+p2 = Person("Mercedes", 24)
+p3 = Person("Beatrice", 48)
+person_list = [p1, p2, p3]
+answer = std_dev(person_list)
+print('Standard Deviation of age:',answer)
